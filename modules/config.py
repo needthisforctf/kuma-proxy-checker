1c409487dada9e65ef5b21384c344e5700aee05b
needthisforctf <111290117+needthisforctf@users.noreply.github.com>
2025-05-26 02:38:01 +0400

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -7,9 +7,9 @@ load_dotenv()
 class Config:
     PROXIES_YAML = os.getenv("PROXIES_YAML")
     JSON_OUT = os.getenv("JSON_OUT")
-    URL = os.getenv("KUMA_URL")
-    USERNAME = os.getenv("KUMA_USERNAME")
-    PASSWORD = os.getenv("KUMA_PASSWORD")
+    KUMA_URL = os.getenv("KUMA_URL")
+    KUMA_USERNAME = os.getenv("KUMA_USERNAME")
+    KUMA_PASSWORD = os.getenv("KUMA_PASSWORD")
 
 
 config = Config()
